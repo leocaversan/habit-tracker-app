49948f6503669d298d35498f5198a4a4d2870098
leoCaversan <leocaversan7@gmail.com>
2023-07-02 21:40:23 -0300

    feat: create component ViewHabit

--- habit-tracker/src/Componentes/ViewHabit/index.tsx
+++ habit-tracker/src/Componentes/ViewHabit/index.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import './index.css'
 
+
 const ViewHabit = () => {
+
+    const [spanVisibleView, setSpanViewVisible] = useState(false);
+    const [spanVisibleDelete, setSpanDeleteVisible] = useState(false);
+
     return (
         <div className="conteiner__viewHabit">
             <div className="conteiner__viewHabit-header">
@@ -14,16 +19,72 @@ const ViewHabit = () => {
                     Habito { }: { }
                 </p>
                 <div>
-                    <button>
-                        <img src="" alt="surch" />
+                    <button
+                        onClick={() => {
+                            setSpanViewVisible(!spanVisibleView);
+                        }}>
+                        <img src="" alt="view" />
                     </button>
                     <button>
                         <img src="" alt="edit" />
                     </button>
-                    <button>
+                    <button onClick={() => {
+                        setSpanDeleteVisible(!spanVisibleDelete);
+                    }}>
                         <img src="" alt="delete" />
                     </button>
                 </div>
+                {spanVisibleView && (
+                    <div className="conteiner__viewHabit-ocult_span">
+                        <div className="conteiner__viewHabit-ocult-span-insideSpan">
+                            <p>
+                                Habito
+                            </p>
+                            <button
+                                onClick={() => {
+                                    setSpanViewVisible(!spanVisibleView);
+                                }}
+                            >
+                                <img src="" alt="edit" />
+                            </button>
+                        </div>
+                        <div>
+                            <p>
+                                Horario do habito
+                            </p>
+                            <button>
+                                16:00
+                            </button>
+                        </div>
+                        <div>
+                            <p>
+                                Periodicidade do habito
+                            </p>
+                            <button>
+                                Seg
+                            </button>
+                        </div>
+                    </div>
+                )
+                }
+
+                {spanVisibleDelete && (
+                    <div className="conteiner__viewHabit-ocult_span">
+                        <div className="conteiner__viewHabit-ocult_span-button">
+                            <h1>
+                                Deseja excluir esse habito?
+                            </h1>
+                            <div className="conteiner__viewHabit-ocult_span-buttons">
+                                <button>
+                                    Sim
+                                </button>
+                                <button>
+                                    Sim
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
