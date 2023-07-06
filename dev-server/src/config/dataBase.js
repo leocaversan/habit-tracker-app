0689573b3018d4904447327eeba987df48dd339b
leoCaversan <leocaversan7@gmail.com>
2023-07-05 22:09:22 -0300

    remove comented code and credentials

--- dev-server/src/config/dataBase.js
+++ dev-server/src/config/dataBase.js
@@ -2,5 +2,4 @@ import dotenv from 'dotenv/config';
 
 export default {
     url:process.env.MONGO_URL,
-    // url:"mongodb://mongo:uL7lLFuE9MO9Kf9EhzZL@containers-us-west-55.railway.app:7510",
-}
+   }
